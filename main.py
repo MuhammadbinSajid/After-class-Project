e1a11bc3da01f75731dec5838db2f16da438aca9
MuhammadbinSajid <mohammedsajidamazai@gmail.com>
2024-08-31 19:50:21 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import math
+
+# Take input from the user
+number = float(input("Enter a number to find the square root: "))
+
+# Calculate the square root
+square_root = math.sqrt(number)
+
+# Display the result
+print(f"The square root of {number} is {square_root}")
